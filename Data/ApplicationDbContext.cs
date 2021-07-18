e59bfe9a9e1b7c33528a8392e8298fce7192cc87
Samuel Terence Narbuada <11615957@benilde.edu.ph>
2021-07-18 22:51:18 +0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using MyInventory.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace MyInventory.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        }
+        
+        public DbSet<Item> Items { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
